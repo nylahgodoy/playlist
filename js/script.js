11fc4a5d07a7e1c558709ab73771d29f7459c57a
Nylah Godoy <ntng08@gmail.com>
2019-02-26 22:56:14 +0000

    playlist1

--- js/script.js
+++ js/script.js
@@ -1,28 +1,52 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["20 Min", "Tetris", "Get You", "XX"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+ var artists = ["Lil Uzi Vert", "Derek King", "Daniel Ceasar", "Marc E. Bassy"];
+ var songLength = ["3:40", "3:48", "4:30", "3:30"];
+ var imgLinks = ["", "", "", ""];
+ var songLink = ["", "", "", ""];
+
+
 
 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
+    songs.forEach(function(song) {
+        $("#songs").append("<div>" + song + "</div>");
+        });
+    artists.forEach(function(artists) {
+        $("#artists").append("<li>" + artists + "</li>");
+        });
+    songLength.forEach(function(length) {
+        $("#lengths").append("<li>" + songLength + "</li>");
+        });
+    imgLinks.forEach(function(img) {
+        $("#images").append("<li>" + img + "</li>");
+        });
+    songLink.forEach(function(links) {
+        $("#links").append("<li>" + links + "</li>");
+        });
 }
 
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
-
+    $("#artist").empty();
+    $("#length").empty();
+    $("#image").empty();
+    $("#link").empty();
 
 }
 
 
 function addSongInfo(){
     // BELOW write the code to add new items to each of the arrays.
-
+    var name = $("#song").val();
+    songs.push(name)
 
 }
 
